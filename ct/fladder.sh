9485bbca104945a1a4d4894f907e0118f37d2272
Wendy Liga <16457495+wendyliga@users.noreply.github.com>
2025-12-27 00:10:54 +0700

    fix: correct version display message in update_script function

--- ct/fladder.sh
+++ ct/fladder.sh
@@ -23,7 +23,7 @@ function update_script() {
   header_info
   check_container_storage
   check_container_resources
-  
+
   if [[ ! -d /opt/fladder ]]; then
     msg_error "No ${APP} Installation Found!"
     exit
@@ -71,7 +71,7 @@ function update_script() {
     msg_ok "Started Service"
     msg_ok "Updated successfully!"
   else
-    msg_ok "No update required. ${APP} is already at v${RELEASE}"
+    msg_ok "No update required. ${APP} is already at ${RELEASE}"
   fi
   exit
 }
